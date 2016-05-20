d0838b52426ec9d1177dc5da22d93c7aa96b4032
Salvatore Festa <valoxblood@gmail.com>
2016-05-20 20:14:32 +0200

    Revert "[2/4] Breathing missedcall/sms/voicemail"
    This reverts commit 31837880c8ae00a4ae509c9490bc50201756cfc8.

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -331,7 +331,6 @@ public class NotificationMgr {
      *
      * @param visible true if there are messages waiting
      */
-
     /* package */ void updateMwi(int subId, boolean visible) {
         updateMwi(subId, visible, true /* enableNotificationSound */);
     }
@@ -368,7 +367,6 @@ public class NotificationMgr {
         Log.i(LOG_TAG, "updateMwi(): subId " + subId + " update to " + visible);
         mMwiVisible.put(subId, visible);
 
-        int resId;
         if (visible) {
             if (phone == null) {
                 Log.w(LOG_TAG, "Found null phone for: " + subId);
@@ -379,18 +377,11 @@ public class NotificationMgr {
             if (subInfo == null) {
                 Log.w(LOG_TAG, "Found null subscription info for: " + subId);
                 return;
-
             }
 
-            if (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1) {
-                resId = R.drawable.stat_notify_voicemail_breath;
-		if (mTelephonyManager.getPhoneCount() > 1) {
-                    resId = mwiIcon[phoneId]; }
-            } else {
-                resId = android.R.drawable.stat_notify_voicemail;
-                if (mTelephonyManager.getPhoneCount() > 1) {
-                    resId = mwiIcon[phoneId]; }
+            int resId = android.R.drawable.stat_notify_voicemail;
+            if (mTelephonyManager.getPhoneCount() > 1) {
+                resId = mwiIcon[phoneId];
             }
 
             // This Notification can get a lot fancier once we have more
