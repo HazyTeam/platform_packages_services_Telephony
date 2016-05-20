0684d91428c4b2cade5a07790584c8addb902b70
Salvatore Festa <valoxblood@gmail.com>
2016-05-20 20:13:45 +0200

    Revert "Telephony: reload CarrierConfig based on changes to ro.turbo.version"
    This reverts commit ff4bcc2944e33677069317c9a4d4e582aa59952b.

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -316,11 +316,11 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.TURBO.equals(lastFingerprint)) {
+                    if (!Build.FINGERPRINT.equals(lastFingerprint)) {
                         log("Build fingerprint changed. old: "
-                                + lastFingerprint + " new: " + Build.TURBO);
+                                + lastFingerprint + " new: " + Build.FINGERPRINT);
                         clearCachedConfigForPackage(null);
-                        sharedPrefs.edit().putString(KEY_FINGERPRINT, Build.TURBO).apply();
+                        sharedPrefs.edit().putString(KEY_FINGERPRINT, Build.FINGERPRINT).apply();
                     }
                     break;
             }
